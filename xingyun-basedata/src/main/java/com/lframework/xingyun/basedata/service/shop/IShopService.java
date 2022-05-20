0550e5101f7a0b02ea2fcaaf1ea13fe914456672
lframework <lframework@163.com>
2022-05-20 09:34:36 +0800

    门店信息

---
+++ xingyun-basedata/src/main/java/com/lframework/xingyun/basedata/service/shop/IShopService.java
@@ -0,0 +1,51 @@
+package com.lframework.xingyun.basedata.service.shop;
+
+import com.lframework.starter.mybatis.resp.PageResult;
+import com.lframework.starter.mybatis.service.BaseMpService;
+import com.lframework.xingyun.basedata.entity.Shop;
+import com.lframework.xingyun.basedata.vo.shop.CreateShopVo;
+import com.lframework.xingyun.basedata.vo.shop.QueryShopVo;
+import com.lframework.xingyun.basedata.vo.shop.UpdateShopVo;
+import java.util.List;
+
+/**
+ * 门店 Service
+ *
+ * @author zmj
+ */
+public interface IShopService extends BaseMpService<Shop> {
+
+  /**
+   * 查询列表
+   * @return
+   */
+  PageResult<Shop> query(Integer pageIndex, Integer pageSize, QueryShopVo vo);
+
+  /**
+   * 查询列表
+   * @param vo
+   * @return
+   */
+  List<Shop> query(QueryShopVo vo);
+
+  /**
+   * 根据ID查询
+   * @param id
+   * @return
+   */
+  Shop findById(String id);
+
+  /**
+   * 创建
+   * @param vo
+   * @return
+   */
+  String create(CreateShopVo vo);
+
+  /**
+   * 修改
+   * @param vo
+   */
+  void update(UpdateShopVo vo);
+
+}
---
+++ xingyun-basedata/src/main/java/com/lframework/xingyun/basedata/vo/shop/QueryShopVo.java
@@ -0,0 +1,57 @@
+package com.lframework.xingyun.basedata.vo.shop;
+
+import com.lframework.starter.web.components.validation.TypeMismatch;
+import com.lframework.starter.web.vo.BaseVo;
+import com.lframework.starter.web.vo.PageVo;
+import io.swagger.annotations.ApiModelProperty;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class QueryShopVo extends PageVo implements BaseVo, Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  /**
+   * 编号
+   */
+  @ApiModelProperty("编号")
+  private String code;
+
+  /**
+   * 名称
+   */
+  @ApiModelProperty("名称")
+  private String name;
+
+  /**
+   * 所属部门ID
+   */
+  @ApiModelProperty("所属部门ID")
+  private String deptId;
+
+  /**
+   * 创建时间 起始时间
+   */
+  @ApiModelProperty("创建时间 起始时间")
+  @TypeMismatch(message = "创建时间起始时间格式有误！")
+  private LocalDateTime createTimeStart;
+
+  /**
+   * 创建时间 截止时间
+   */
+  @ApiModelProperty("创建时间 截止时间")
+  @TypeMismatch(message = "创建时间截止时间格式有误！")
+  private LocalDateTime createTimeEnd;
+
+  /**
+   * 状态
+   */
+  @ApiModelProperty("状态")
+  @TypeMismatch(message = "状态格式有误！")
+  private Boolean available;
+
+}

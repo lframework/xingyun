fd2ac9ea1432ec26a67e539de5d505d84f1930d4
lframework <lframework@163.com>
2022-06-24 23:23:26 +0800

    update

---
+++ xingyun-api/src/main/resources/db/migration/V1.18__excel_import2.sql
@@ -0,0 +1,6 @@
+INSERT INTO `sys_menu` (`id`, `code`, `name`, `title`, `component`, `parent_id`, `path`, `no_cache`,
+                        `display`, `hidden`, `permission`, `is_special`, `available`, `description`,
+                        `create_by`, `create_time`, `update_by`, `update_time`)
+VALUES ('2000006003', '2000006003', '', '导入会员', '', '2000006', '', 0, 2, 0,
+        'base-data:member:import', 1, 1, '', '1', '2021-05-12 23:23:33', '1',
+        '2021-07-04 00:34:23');

52d6750087c6ce0c9732815fd17f926e55aaaa48
lframework <lframework@163.com>
2022-06-24 23:21:13 +0800

    update

--- xingyun-api/src/main/java/com/lframework/xingyun/api/controller/basedata/member/MemberController.java
+++ xingyun-api/src/main/java/com/lframework/xingyun/api/controller/basedata/member/MemberController.java
@@ -8,8 +8,11 @@ import com.lframework.starter.security.controller.DefaultBaseController;
 import com.lframework.starter.web.resp.InvokeResult;
 import com.lframework.starter.web.resp.InvokeResultBuilder;
 import com.lframework.starter.web.utils.ApplicationUtil;
+import com.lframework.starter.web.utils.ExcelUtil;
 import com.lframework.xingyun.api.bo.basedata.member.GetMemberBo;
 import com.lframework.xingyun.api.bo.basedata.member.QueryMemberBo;
+import com.lframework.xingyun.api.excel.basedata.member.MemberImportListener;
+import com.lframework.xingyun.api.excel.basedata.member.MemberImportModel;
 import com.lframework.xingyun.basedata.entity.Member;
 import com.lframework.xingyun.basedata.service.member.IMemberService;
 import com.lframework.xingyun.basedata.vo.member.CreateMemberVo;
@@ -26,6 +29,7 @@ import java.util.stream.Collectors;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
@@ -36,6 +40,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * 会员管理
@@ -48,117 +53,137 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/basedata/member")
 public class MemberController extends DefaultBaseController {
 
-    @Autowired
-    private IMemberService memberService;
+  @Autowired
+  private IMemberService memberService;
 
-    /**
-     * 会员列表
-     */
-    @ApiOperation("会员列表")
-    @PreAuthorize("@permission.valid('base-data:member:query','base-data:member:add','base-data:member:modify')")
-    @GetMapping("/query")
-    public InvokeResult<PageResult<QueryMemberBo>> query(@Valid QueryMemberVo vo) {
+  /**
+   * 会员列表
+   */
+  @ApiOperation("会员列表")
+  @PreAuthorize("@permission.valid('base-data:member:query','base-data:member:add','base-data:member:modify')")
+  @GetMapping("/query")
+  public InvokeResult<PageResult<QueryMemberBo>> query(@Valid QueryMemberVo vo) {
 
-        PageResult<Member> pageResult = memberService.query(getPageIndex(vo), getPageSize(vo), vo);
+    PageResult<Member> pageResult = memberService.query(getPageIndex(vo), getPageSize(vo), vo);
 
-        List<Member> datas = pageResult.getDatas();
-        List<QueryMemberBo> results = null;
+    List<Member> datas = pageResult.getDatas();
+    List<QueryMemberBo> results = null;
 
-        if (!CollectionUtil.isEmpty(datas)) {
-            results = datas.stream().map(QueryMemberBo::new).collect(Collectors.toList());
-        }
-
-        return InvokeResultBuilder.success(PageResultUtil.rebuild(pageResult, results));
+    if (!CollectionUtil.isEmpty(datas)) {
+      results = datas.stream().map(QueryMemberBo::new).collect(Collectors.toList());
     }
 
-    /**
-     * 查询会员
-     */
-    @ApiOperation("查询会员")
-    @ApiImplicitParam(value = "ID", name = "id", paramType = "query", required = true)
-    @PreAuthorize("@permission.valid('base-data:member:query','base-data:member:add','base-data:member:modify')")
-    @GetMapping
-    public InvokeResult<GetMemberBo> get(@NotBlank(message = "ID不能为空！") String id) {
+    return InvokeResultBuilder.success(PageResultUtil.rebuild(pageResult, results));
+  }
 
-        Member data = memberService.findById(id);
-        if (data == null) {
-            throw new DefaultClientException("会员不存在！");
-        }
+  /**
+   * 查询会员
+   */
+  @ApiOperation("查询会员")
+  @ApiImplicitParam(value = "ID", name = "id", paramType = "query", required = true)
+  @PreAuthorize("@permission.valid('base-data:member:query','base-data:member:add','base-data:member:modify')")
+  @GetMapping
+  public InvokeResult<GetMemberBo> get(@NotBlank(message = "ID不能为空！") String id) {
 
-        GetMemberBo result = new GetMemberBo(data);
-
-        return InvokeResultBuilder.success(result);
+    Member data = memberService.findById(id);
+    if (data == null) {
+      throw new DefaultClientException("会员不存在！");
     }
 
-    /**
-     * 批量停用会员
-     */
-    @ApiOperation("批量停用会员")
-    @PreAuthorize("@permission.valid('base-data:member:modify')")
-    @PatchMapping("/unable/batch")
-    public InvokeResult<Void> batchUnable(
-            @ApiParam(value = "ID", required = true) @NotEmpty(message = "请选择需要停用的会员！") @RequestBody List<String> ids) {
+    GetMemberBo result = new GetMemberBo(data);
 
-        memberService.batchUnable(ids);
+    return InvokeResultBuilder.success(result);
+  }
 
-        for (String id : ids) {
-            memberService.cleanCacheByKey(id);
-        }
+  /**
+   * 批量停用会员
+   */
+  @ApiOperation("批量停用会员")
+  @PreAuthorize("@permission.valid('base-data:member:modify')")
+  @PatchMapping("/unable/batch")
+  public InvokeResult<Void> batchUnable(
+      @ApiParam(value = "ID", required = true) @NotEmpty(message = "请选择需要停用的会员！") @RequestBody List<String> ids) {
 
-        return InvokeResultBuilder.success();
+    memberService.batchUnable(ids);
+
+    for (String id : ids) {
+      memberService.cleanCacheByKey(id);
     }
 
-    /**
-     * 批量启用会员
-     */
-    @ApiOperation("批量启用会员")
-    @PreAuthorize("@permission.valid('base-data:member:modify')")
-    @PatchMapping("/enable/batch")
-    public InvokeResult<Void> batchEnable(
-            @ApiParam(value = "ID", required = true) @NotEmpty(message = "请选择需要启用的会员！") @RequestBody List<String> ids) {
+    return InvokeResultBuilder.success();
+  }
 
-        memberService.batchEnable(ids);
+  /**
+   * 批量启用会员
+   */
+  @ApiOperation("批量启用会员")
+  @PreAuthorize("@permission.valid('base-data:member:modify')")
+  @PatchMapping("/enable/batch")
+  public InvokeResult<Void> batchEnable(
+      @ApiParam(value = "ID", required = true) @NotEmpty(message = "请选择需要启用的会员！") @RequestBody List<String> ids) {
 
-        for (String id : ids) {
-            memberService.cleanCacheByKey(id);
-        }
+    memberService.batchEnable(ids);
 
-        return InvokeResultBuilder.success();
+    for (String id : ids) {
+      memberService.cleanCacheByKey(id);
     }
 
-    /**
-     * 新增会员
-     */
-    @ApiOperation("新增会员")
-    @PreAuthorize("@permission.valid('base-data:member:add')")
-    @PostMapping
-    public InvokeResult<Void> create(@Valid CreateMemberVo vo) {
+    return InvokeResultBuilder.success();
+  }
 
-        String id = memberService.create(vo);
+  /**
+   * 新增会员
+   */
+  @ApiOperation("新增会员")
+  @PreAuthorize("@permission.valid('base-data:member:add')")
+  @PostMapping
+  public InvokeResult<Void> create(@Valid CreateMemberVo vo) {
 
-        CreateMemberEvent event = new CreateMemberEvent(this);
-        event.setId(id);
-        ApplicationUtil.publishEvent(event);
+    String id = memberService.create(vo);
 
-        return InvokeResultBuilder.success();
-    }
+    CreateMemberEvent event = new CreateMemberEvent(this);
+    event.setId(id);
+    ApplicationUtil.publishEvent(event);
 
-    /**
-     * 修改会员
-     */
-    @ApiOperation("修改会员")
-    @PreAuthorize("@permission.valid('base-data:member:modify')")
-    @PutMapping
-    public InvokeResult<Void> update(@Valid UpdateMemberVo vo) {
+    return InvokeResultBuilder.success();
+  }
 
-        memberService.update(vo);
+  /**
+   * 修改会员
+   */
+  @ApiOperation("修改会员")
+  @PreAuthorize("@permission.valid('base-data:member:modify')")
+  @PutMapping
+  public InvokeResult<Void> update(@Valid UpdateMemberVo vo) {
 
-        memberService.cleanCacheByKey(vo.getId());
+    memberService.update(vo);
 
-        UpdateMemberEvent event = new UpdateMemberEvent(this);
-        event.setId(vo.getId());
-        ApplicationUtil.publishEvent(event);
+    memberService.cleanCacheByKey(vo.getId());
 
-        return InvokeResultBuilder.success();
-    }
+    UpdateMemberEvent event = new UpdateMemberEvent(this);
+    event.setId(vo.getId());
+    ApplicationUtil.publishEvent(event);
+
+    return InvokeResultBuilder.success();
+  }
+
+  @ApiOperation("下载导入模板")
+  @PreAuthorize("@permission.valid('base-data:member:import')")
+  @GetMapping("/import/template")
+  public void downloadImportTemplate() {
+    ExcelUtil.exportXls("会员导入模板", MemberImportModel.class);
+  }
+
+  @ApiOperation("导入")
+  @PreAuthorize("@permission.valid('base-data:member:import')")
+  @PostMapping("/import")
+  public InvokeResult<Void> importExcel(@NotBlank(message = "ID不能为空") String id,
+      @NotNull(message = "请上传文件") MultipartFile file) {
+
+    MemberImportListener listener = new MemberImportListener();
+    listener.setTaskId(id);
+    ExcelUtil.read(file, MemberImportModel.class, listener).sheet().doRead();
+
+    return InvokeResultBuilder.success();
+  }
 }
---
+++ xingyun-api/src/main/java/com/lframework/xingyun/api/excel/basedata/member/MemberImportListener.java
@@ -0,0 +1,146 @@
+package com.lframework.xingyun.api.excel.basedata.member;
+
+import com.alibaba.excel.context.AnalysisContext;
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.lframework.common.constants.PatternPool;
+import com.lframework.common.exceptions.impl.DefaultClientException;
+import com.lframework.common.utils.DateUtil;
+import com.lframework.common.utils.IdUtil;
+import com.lframework.common.utils.RegUtil;
+import com.lframework.common.utils.StringUtil;
+import com.lframework.starter.mybatis.components.excel.ExcelImportListener;
+import com.lframework.starter.mybatis.entity.DefaultSysUser;
+import com.lframework.starter.mybatis.enums.Gender;
+import com.lframework.starter.mybatis.service.IUserService;
+import com.lframework.starter.web.utils.ApplicationUtil;
+import com.lframework.xingyun.basedata.entity.Member;
+import com.lframework.xingyun.basedata.entity.Shop;
+import com.lframework.xingyun.basedata.service.member.IMemberService;
+import com.lframework.xingyun.basedata.service.shop.IShopService;
+import com.lframework.xingyun.core.events.member.CreateMemberEvent;
+import com.lframework.xingyun.core.events.member.UpdateMemberEvent;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MemberImportListener extends ExcelImportListener<MemberImportModel> {
+
+  @Override
+  protected void doInvoke(MemberImportModel data, AnalysisContext context) {
+    if (StringUtil.isBlank(data.getCode())) {
+      throw new DefaultClientException("第" + context.readRowHolder().getRowIndex() + "行“编号”不能为空");
+    }
+    if (!RegUtil.isMatch(PatternPool.PATTERN_CODE, data.getCode())) {
+      throw new DefaultClientException(
+          "第" + context.readRowHolder().getRowIndex() + "行“编号”必须由字母、数字、“-_.”组成，长度不能超过20位");
+    }
+    if (StringUtil.isBlank(data.getName())) {
+      throw new DefaultClientException("第" + context.readRowHolder().getRowIndex() + "行“名称”不能为空");
+    }
+    if (StringUtil.isBlank(data.getGender())) {
+      throw new DefaultClientException("第" + context.readRowHolder().getRowIndex() + "行“性别”不能为空");
+    }
+
+    if (!Gender.UNKNOWN.getDesc().equals(data.getGender()) && !Gender.MAN.getDesc()
+        .equals(data.getGender()) && !Gender.FEMALE.getDesc().equals(data.getGender())) {
+      throw new DefaultClientException(
+          "第" + context.readRowHolder().getRowIndex() + "行“性别”只能填写“" + Gender.UNKNOWN.getDesc()
+              + "、" + Gender.MAN.getDesc() + "、" + Gender.FEMALE.getDesc() + "”");
+    }
+
+    if (Gender.UNKNOWN.getDesc().equals(data.getGender())) {
+      data.setGenderEnum(Gender.UNKNOWN);
+    } else if (Gender.MAN.getDesc().equals(data.getGender())) {
+      data.setGenderEnum(Gender.MAN);
+    } else if (Gender.FEMALE.getDesc().equals(data.getGender())) {
+      data.setGenderEnum(Gender.FEMALE);
+    }
+
+    if (!StringUtil.isEmpty(data.getEmail())) {
+      if (!RegUtil.isMatch(PatternPool.EMAIL, data.getEmail())) {
+        throw new DefaultClientException(
+            "第" + context.readRowHolder().getRowIndex() + "行“电子邮箱”格式有误");
+      }
+    }
+
+    if (!StringUtil.isEmpty(data.getShopCode())) {
+      IShopService shopService = ApplicationUtil.getBean(IShopService.class);
+      Wrapper<Shop> queryWrapper = Wrappers.lambdaQuery(Shop.class)
+          .eq(Shop::getCode, data.getShopCode());
+      Shop shop = shopService.getOne(queryWrapper);
+      if (shop == null) {
+        throw new DefaultClientException(
+            "第" + context.readRowHolder().getRowIndex() + "行“所属门店编号”不存在");
+      }
+      data.setShopId(shop.getId());
+    }
+
+    if (!StringUtil.isEmpty(data.getGuiderCode())) {
+      IUserService userService = ApplicationUtil.getBean(IUserService.class);
+      Wrapper<DefaultSysUser> queryWrapper = Wrappers.lambdaQuery(DefaultSysUser.class)
+          .eq(DefaultSysUser::getCode, data.getGuiderCode());
+      DefaultSysUser guider = userService.getOne(queryWrapper);
+      if (guider == null) {
+        throw new DefaultClientException(
+            "第" + context.readRowHolder().getRowIndex() + "行“所属导购编号”不存在");
+      }
+      data.setGuiderId(guider.getId());
+    }
+  }
+
+  @Override
+  protected void afterAllAnalysed(AnalysisContext context) {
+
+    IMemberService memberService = ApplicationUtil.getBean(IMemberService.class);
+
+    List<MemberImportModel> datas = this.getDatas();
+    for (MemberImportModel data : datas) {
+
+      boolean isInsert = false;
+      Wrapper<Member> queryWrapper = Wrappers.lambdaQuery(Member.class)
+          .eq(Member::getCode, data.getCode());
+      Member record = memberService.getOne(queryWrapper);
+      if (record == null) {
+        record = new Member();
+        record.setId(IdUtil.getId());
+        isInsert = true;
+      }
+
+      record.setCode(data.getCode());
+      record.setName(data.getName());
+      record.setGender(data.getGenderEnum());
+      record.setTelephone(data.getTelephone());
+      record.setEmail(data.getEmail());
+      record.setBirthday(DateUtil.toLocalDate(data.getBirthday()));
+      record.setJoinDay(DateUtil.toLocalDate(data.getJoinDay()));
+      record.setShopId(data.getShopId());
+      record.setGuiderId(data.getGuiderId());
+      record.setDescription(data.getDescription());
+      if (isInsert) {
+        record.setAvailable(Boolean.TRUE);
+      }
+
+      data.setId(record.getId());
+      data.setIsInsert(isInsert);
+
+      memberService.saveOrUpdate(record);
+    }
+  }
+
+  @Override
+  protected void doComplete() {
+    List<MemberImportModel> datas = this.getDatas();
+    for (MemberImportModel data : datas) {
+      if (data.getIsInsert()) {
+        CreateMemberEvent event = new CreateMemberEvent(this);
+        event.setId(data.getId());
+        ApplicationUtil.publishEvent(event);
+      } else {
+        UpdateMemberEvent event = new UpdateMemberEvent(this);
+        event.setId(data.getId());
+        ApplicationUtil.publishEvent(event);
+      }
+    }
+  }
+}
